b0fcf8853be8ae34f2ebb2891d1ae4417a1f7d22
Rasul Kireev <me@rasulkireev.com>
2024-09-21 12:53:35 +0300

    update settings module

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -12,7 +12,7 @@ fi
 export PROJECT_NAME=isitketo
 export OTEL_EXPORTER_OTLP_ENDPOINT=https://signoz-otel-collector-proxy.cr.lvtd.dev
 export OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
-export DJANGO_SETTINGS_MODULE="isitketo.settings.production"
+export DJANGO_SETTINGS_MODULE="isitketo.settings"
 
 while getopts ":sw" option; do
     case "${option}" in
